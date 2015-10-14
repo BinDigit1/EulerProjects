9e6c744b0ddd8ec28c0ce47eeb48982afafbfa07
Konstantinos Vasileiou <bindigit1@gmail.com>
2015-10-14 16:11:20 +0300

    initial commit

--- Largest_prime_factor.py
+++ Largest_prime_factor.py
@@ -3,19 +3,26 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 
 '''
+from math import sqrt
 UPPER_LIMIT = 600851475143
 my_primes = []
-for x in range(2, UPPER_LIMIT//2):
+x=3
+while(x<UPPER_LIMIT//2):
+
     if (UPPER_LIMIT%x ==0):
         print("Potential prime: ", x)
         #potential prime - check with previous primes in set
         is_prime = True
         for prime in my_primes:
-            if (x%prime == 0 and prime <= x//2):
+            if (prime>x//2):
+                break
+            elif (x%prime == 0):
                 is_prime = False
                 break
+
         if (is_prime):
             my_primes.append(x)
             print("added prime: ", x)
+    x+=2
 
 print(my_primes)
