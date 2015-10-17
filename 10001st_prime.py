be84275748310c3ae8a871db2662827eafdec3a6
BinDigit1 <bindigit1@gmail.com>
2015-10-17 15:24:02 +0300

    initial commit

--- 10001st_prime.py
+++ 10001st_prime.py
@@ -2,23 +2,32 @@
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 What is the 10 001st prime number?
 '''
+from math import sqrt
 def checkIfPrime(i):
-    isPrime = True
-    for num in range(2,i//2+1):
-        #print(num)
-        if(i%num == 0):
-            isPrime = False
-            break
-    return isPrime
+    if(i == 2):
+        return True
+    elif(i%2 == 0):
+        return False
+    else:
+        isPrime = True
+        for num in range(3,int(sqrt(i)+1)):
+            #print(num)
+            if(i%num == 0):
+                isPrime = False
+                break
+        return isPrime
 
 def primeCounter(orderInPrimes):
     currentNum = 2
-    listOfPrimes = list()
-    while(len(listOfPrimes)<orderInPrimes):
+    currentOrderInPrimes=0
+    while True:
         if(checkIfPrime(currentNum)):
-            listOfPrimes.append(currentNum)
-        currentNum+=1
-    print(listOfPrimes[-1])
+            currentOrderInPrimes+=1
+        if(currentOrderInPrimes<orderInPrimes):
+            currentNum+=1
+        else:
+            break
+    print(currentNum)
 
 
 
