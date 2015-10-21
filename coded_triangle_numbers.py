74eab7842d87cfdfe79e59720dff975e406a8d71
Konstantinos Vasileiou <bindigit1@gmail.com>
2015-10-21 14:38:14 +0300

    initial commit

--- coded_triangle_numbers.py
+++ coded_triangle_numbers.py
@@ -4,20 +4,23 @@ The nth term of the sequence of triangle numbers is given by, tn = 0.5n(n+1); so
 By converting each letter in a word to a number corresponding to its alphabetical position and adding these values we form a word value. For example, the word value for SKY is 19 + 11 + 25 = 55 = t10. If the word value is a triangle number then we shall call the word a triangle word.
 Using words.txt (right click and 'Save Link/Target As...'), a 16K text file containing nearly two-thousand common English words, how many are triangle words?
 '''
-import math
+import math, time
 
 
 def is_triangle_num(number):
     delta = 1 + 8*number
-    root = ((-1)+math.sqrt(delta))/2
+    root = ((-1)+math.sqrt(delta))/2 #only the positive root is interesting for our case
     intRoot = int(root)
 
-    if(intRoot==root):
-        print(delta, root, number)
+    if(intRoot==root): #checking if root is a natural number
         return True
     else:
 
         return False
+
+
+start_time = time.time()
+
 
 text_file = open("p042_words.txt", "r")
 lines = text_file.read().strip('"').split('","')
@@ -27,9 +30,10 @@ for word in lines:
 
     sum = 0
     for c in word:
-        sum+=ord(c)-ord('A')+1
+        sum+=ord(c)-ord('A')+1 #calculating the "numeric" value of a character - assuming that A is 1
     if is_triangle_num(sum):
         number_of_triangle+=1
 print("result: ", number_of_triangle)
 
+print("--- %s seconds ---" % (time.time() - start_time))
 
