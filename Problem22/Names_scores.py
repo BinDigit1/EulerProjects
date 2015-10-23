e56649483ada655f09ff98d6cd10e236cf87ae2c
Konstantinos Vasileiou <bindigit1@gmail.com>
2015-10-23 15:04:55 +0300

    initial commit

--- Problem22/Names_scores.py
+++ Problem22/Names_scores.py
@@ -1,5 +1,23 @@
-
+import time
 fo = open("p022_names.txt",'r')
-names =fo.read().strip('"').split('","').sort()
+names =sorted(fo.read().strip('"').split('","'))
 
+'''
+COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53
+'''
+def score(word):
+    sum = 0
+    for i in word:
+
+        sum += int(i)-ord('A')+1
+    return sum
+
+
+start_time = time.time()
+sum, cursor = 0, 1
+for i in names:
+    sum += score(i) * cursor
+    cursor+=1
 print(names)
+print(score('COLIN'))
+print("--- %s seconds ---" % (time.time() - start_time))
