54cb1e47a5db250d14adfd8a207959fce4b6d919
BinDigit1 <bindigit1@gmail.com>
2015-10-24 11:24:31 +0300

    Merge branch 'master' of https://github.com/BinDigit1/EulerProjects
    # Conflicts:
    #	10-substrings.py

--- 10001st_prime.py
+++ 10001st_prime.py
@@ -3,31 +3,20 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 What is the 10 001st prime number?
 '''
 from math import sqrt
+from MathFunctions import checkIfPrime
 import time
 
 
-def checkIfPrime(i):
-    if(i == 2):
-        return True
-    elif(i%2 == 0):
-        return False
-    else:
-        isPrime = True
-        for num in range(3,int(sqrt(i)+1),2):
-            #print(num)
-            if(i%num == 0):
-                isPrime = False
-                break
-        return isPrime
+
 
 def primeCounter(orderInPrimes):
     currentNum = 2
     currentOrderInPrimes=0
     while True:
-        if(checkIfPrime(currentNum)):
+        if checkIfPrime(currentNum):
             currentOrderInPrimes+=1
-        if(currentOrderInPrimes<orderInPrimes):
-            currentNum+=1
+        if currentOrderInPrimes<orderInPrimes:
+            currentNum += 1
         else:
             break
     print(currentNum)
