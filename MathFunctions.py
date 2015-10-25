45c6290535f4ea5d1ad300a057f02f6044418725
BinDigit1 <bindigit1@gmail.com>
2015-10-25 12:09:01 +0200

    Merge branch 'master' of https://github.com/BinDigit1/EulerProjects
    # Conflicts:
    #	10-substrings.py

--- MathFunctions.py
+++ MathFunctions.py
@@ -1,54 +1,62 @@
 __author__ = 'k.vasileiou'
 import math
 
+
 def checkIfPrime(i):
-    if(i == 2):
+    if (i == 2):
         return True
-    elif(i%2 == 0):
+    elif (i % 2 == 0):
         return False
     else:
         isPrime = True
-        for num in range(3,int(math.sqrt(i)+1),2):
-            #print(num)
-            if(i%num == 0):
+        for num in range(3, int(math.sqrt(i) + 1), 2):
+            # print(num)
+            if (i % num == 0):
                 isPrime = False
                 break
         return isPrime
 
+
 def triangular(number):
-    return number*(number+1)/2
+    return number * (number + 1) / 2
+
 
 def inverse_triangular(number):
-    '''
+    """
     Checks if a number is triangular, i.e. if there exists a natural n for which T(n) = number
     :param number: the number to be checked
     :return: true if the given number is triangular, false otherwise.
-    '''
-    delta = 1+8*number
-    root = (-1 + math.sqrt(delta))/2
+    """
+    delta = 1 + 8 * number
+    root = (-1 + math.sqrt(delta)) / 2
 
-    return root==int(root)
+    return root == int(root)
+
 
 def pentagonal(number):
-    return number*(3*number-1)/2
+    return number * (3 * number - 1) / 2
+
 
 def inverse_pentagonal(number):
-    delta = 1+24*number
-    root = (1 + math.sqrt(delta))/6
+    delta = 1 + 24 * number
+    root = (1 + math.sqrt(delta)) / 6
 
-    return root==int(root)
+    return root == int(root)
+
 
 def hexagonal(number):
-    return number*(2*number - 1)
+    return number * (2 * number - 1)
+
 
 def inverse_hexagonal(number):
-    delta = 1+8*number
-    root = (1 + math.sqrt(delta))/4
+    delta = 1 + 8 * number
+    root = (1 + math.sqrt(delta)) / 4
 
-    return root==int(root)
+    return root == int(root)
+
 
 def fibonacci(n):
-    if(n == 1 or n == 2):
+    if (n == 1 or n == 2):
         return 1
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
