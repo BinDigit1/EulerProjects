d5c636568b3ec408324de9b8ff142e1867cc93f1
BinDigit1 <bindigit1@gmail.com>
2015-10-25 14:02:00 +0200

    Merge branch 'master' of https://github.com/BinDigit1/EulerProjects
    # Conflicts:
    #	10-substrings.py

--- 10001st_prime.py
+++ 10001st_prime.py
@@ -2,25 +2,29 @@
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 What is the 10 001st prime number?
 """
-from math import sqrt
+
 from MathFunctions import checkIfPrime
-import time
+import time, unittest
 
 
-def primeCounter(orderInPrimes):
-    currentNum = 2
-    currentOrderInPrimes = 0
+def prime_counter(order_in_primes):
+    current_num = 2
+    current_order_in_primes = 0
     while True:
-        if checkIfPrime(currentNum):
-            currentOrderInPrimes += 1
-        if currentOrderInPrimes < orderInPrimes:
-            currentNum += 1
+        if checkIfPrime(current_num):
+            current_order_in_primes += 1
+        if current_order_in_primes < order_in_primes:
+            current_num += 1
         else:
             break
-    print(currentNum)
+    return (current_num)
+
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(prime_counter(6), 13)
 
 
 start_time = time.time()
-
-primeCounter(10001)
+print(prime_counter(10001))
 print("--- %s seconds ---" % (time.time() - start_time))
