f7408ee1f8ed0a87f39dcabea1b2d910009918c6
BinDigit1 <bindigit1@gmail.com>
2015-10-27 08:29:39 +0200

    Merge branch 'master' of https://github.com/BinDigit1/EulerProjects
    # Conflicts:
    #	10-substrings.py

--- Problem 28/number_spiral_diagonals.py
+++ Problem 28/number_spiral_diagonals.py
@@ -14,33 +14,30 @@ move_x, move_y = 0, -1
 current_dir = Direction.left
 for i in range(25,0,-1):
     spiral[current_x][current_y] = i
-    if spiral[current_x+move_x][current_y+move_y] == 0:
-        current_x += move_x
-        current_y += move_y
-
-
-    if (current_y == 0 ) and current_dir == Direction.left:
+    if current_dir == Direction.left and (current_y+move_y<0 or spiral[current_x+move_x][current_y+move_y] != 0):
+        print('turn down')
         move_x = 1
         move_y = 0
         current_dir = Direction.down
-        print('moving down')
-    elif (current_x == 4 ) and current_dir == Direction.down:
+
+    elif current_dir == Direction.down and (current_x+move_x>4 or spiral[current_x+move_x][current_y+move_y] != 0):
         move_x = 0
         move_y = 1
         current_dir = Direction.right
         print('moving right')
-    elif (current_y == 4 ) and current_dir == Direction.right:
+    elif current_dir == Direction.right and (current_y+move_y>4 or spiral[current_x+move_x][current_y+move_y] != 0):
         move_x = -1
         move_y = 0
         current_dir = Direction.up
 
         print('moving up')
-    elif (current_x == 0) and current_dir == Direction.up:
+    elif current_dir == Direction.up and (current_x+move_x<0 or spiral[current_x+move_x][current_y+move_y] != 0):
         move_x = 0
         move_y = -1
         current_dir = Direction.left
         print('moving left')
-
+    current_x+=move_x
+    current_y+=move_y
     pprint.pprint(spiral)
     print(current_x,current_y,move_x,move_y, spiral[current_x][current_y], current_dir)
 
