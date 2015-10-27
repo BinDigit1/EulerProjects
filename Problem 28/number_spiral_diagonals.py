f6f52550346f0822b5a5b497ed6cf68276982242
Konstantinos Vasileiou <bindigit1@gmail.com>
2015-10-27 12:32:07 +0200

    commiting changes

--- Problem 28/number_spiral_diagonals.py
+++ Problem 28/number_spiral_diagonals.py
@@ -8,12 +8,12 @@ class Direction(Enum):
 
 
 def create_spiral(size):
-    spiral = [[0 for i in range(5)] for j in range(5)]
-    current_x, current_y = 0, 4
+    spiral = [[0 for i in range(size)] for j in range(size)]
+    current_x, current_y = 0, size-1
     # initially moving to the left
     move_x, move_y = 0, -1
     current_dir = Direction.left
-    for i in range(25, 0, -1):
+    for i in range(size*size, 0, -1):
         spiral[current_x][current_y] = i
         if current_dir == Direction.left and (
                     current_y + move_y < 0 or spiral[current_x + move_x][current_y + move_y] != 0):
@@ -21,12 +21,12 @@ def create_spiral(size):
             move_y = 0
             current_dir = Direction.down
         elif current_dir == Direction.down and (
-                    current_x + move_x > 4 or spiral[current_x + move_x][current_y + move_y] != 0):
+                    current_x + move_x > size-1 or spiral[current_x + move_x][current_y + move_y] != 0):
             move_x = 0
             move_y = 1
             current_dir = Direction.right
         elif current_dir == Direction.right and (
-                    current_y + move_y > 4 or spiral[current_x + move_x][current_y + move_y] != 0):
+                    current_y + move_y > size-1 or spiral[current_x + move_x][current_y + move_y] != 0):
             move_x = -1
             move_y = 0
             current_dir = Direction.up
@@ -37,11 +37,11 @@ def create_spiral(size):
             current_dir = Direction.left
         current_x += move_x
         current_y += move_y
-        pprint.pprint(spiral)
-        print(current_x, current_y, move_x, move_y, spiral[current_x][current_y], current_dir)
+        # pprint.pprint(spiral)
+        # print(current_x, current_y, move_x, move_y, spiral[current_x][current_y], current_dir)
+    return spiral
 
-
-create_spiral()
+create_spiral(10)
 
 
 
