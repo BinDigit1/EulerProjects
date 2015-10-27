ff4a7e4060cbffabb4f93434dd390c21d00ca245
Konstantinos Vasileiou <bindigit1@gmail.com>
2015-10-27 10:37:28 +0200

    commiting changes

--- Problem 28/number_spiral_diagonals.py
+++ Problem 28/number_spiral_diagonals.py
@@ -6,44 +6,34 @@ class Direction(Enum):
     up = 3
     right = 4
 
-spiral = [[None for i in range(5)] for j in range(5)]
+spiral = [[0 for i in range(5)] for j in range(5)]
 
 current_x, current_y = 0, 4
 #initially moving to the left
 move_x, move_y = 0, -1
 current_dir = Direction.left
 for i in range(25,0,-1):
-
-    if (current_y == 0 or spiral[current_x][current_y] != 0) and current_dir == Direction.left:
+    spiral[current_x][current_y] = i
+    if current_dir == Direction.left and (current_y+move_y<0 or spiral[current_x+move_x][current_y+move_y] != 0):
         move_x = 1
         move_y = 0
         current_dir = Direction.down
-        print('moving down')
-
-
-    elif (current_x == 4 or spiral[current_x][current_y] != 0) and current_dir == Direction.down:
+    elif current_dir == Direction.down and (current_x+move_x>4 or spiral[current_x+move_x][current_y+move_y] != 0):
         move_x = 0
         move_y = 1
         current_dir = Direction.right
-        print('moving right')
-
-    elif (current_y == 4 or spiral[current_x][current_y] != 0) and current_dir == Direction.right:
+    elif current_dir == Direction.right and (current_y+move_y>4 or spiral[current_x+move_x][current_y+move_y] != 0):
         move_x = -1
         move_y = 0
         current_dir = Direction.up
-
-        print('moving up')
-
-    elif (current_x == 0 or spiral[current_x][current_y] != 0) and current_dir == Direction.up:
+    elif current_dir == Direction.up and (current_x+move_x<0 or spiral[current_x+move_x][current_y+move_y] != 0):
         move_x = 0
         move_y = -1
         current_dir = Direction.left
-        print('moving left')
-
-    spiral[current_x][current_y] = i
-    current_x += move_x
-    current_y += move_y
-
-
+    current_x+=move_x
+    current_y+=move_y
     pprint.pprint(spiral)
-    print(current_x,current_y,move_x,move_y, spiral[current_x][current_y])
+    print(current_x,current_y,move_x,move_y, spiral[current_x][current_y], current_dir)
+
+
+
