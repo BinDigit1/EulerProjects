ab4a4cec69d65727f15f3525d6e043e2e7857de5
Konstantinos Vasileiou <bindigit1@gmail.com>
2015-10-27 10:39:26 +0200

    commiting changes

--- Problem 28/number_spiral_diagonals.py
+++ Problem 28/number_spiral_diagonals.py
@@ -6,34 +6,42 @@ class Direction(Enum):
     up = 3
     right = 4
 
-spiral = [[0 for i in range(5)] for j in range(5)]
 
-current_x, current_y = 0, 4
-#initially moving to the left
-move_x, move_y = 0, -1
-current_dir = Direction.left
-for i in range(25,0,-1):
-    spiral[current_x][current_y] = i
-    if current_dir == Direction.left and (current_y+move_y<0 or spiral[current_x+move_x][current_y+move_y] != 0):
-        move_x = 1
-        move_y = 0
-        current_dir = Direction.down
-    elif current_dir == Direction.down and (current_x+move_x>4 or spiral[current_x+move_x][current_y+move_y] != 0):
-        move_x = 0
-        move_y = 1
-        current_dir = Direction.right
-    elif current_dir == Direction.right and (current_y+move_y>4 or spiral[current_x+move_x][current_y+move_y] != 0):
-        move_x = -1
-        move_y = 0
-        current_dir = Direction.up
-    elif current_dir == Direction.up and (current_x+move_x<0 or spiral[current_x+move_x][current_y+move_y] != 0):
-        move_x = 0
-        move_y = -1
-        current_dir = Direction.left
-    current_x+=move_x
-    current_y+=move_y
-    pprint.pprint(spiral)
-    print(current_x,current_y,move_x,move_y, spiral[current_x][current_y], current_dir)
+def create_spiral(size):
+    spiral = [[0 for i in range(5)] for j in range(5)]
+    current_x, current_y = 0, 4
+    # initially moving to the left
+    move_x, move_y = 0, -1
+    current_dir = Direction.left
+    for i in range(25, 0, -1):
+        spiral[current_x][current_y] = i
+        if current_dir == Direction.left and (
+                    current_y + move_y < 0 or spiral[current_x + move_x][current_y + move_y] != 0):
+            move_x = 1
+            move_y = 0
+            current_dir = Direction.down
+        elif current_dir == Direction.down and (
+                    current_x + move_x > 4 or spiral[current_x + move_x][current_y + move_y] != 0):
+            move_x = 0
+            move_y = 1
+            current_dir = Direction.right
+        elif current_dir == Direction.right and (
+                    current_y + move_y > 4 or spiral[current_x + move_x][current_y + move_y] != 0):
+            move_x = -1
+            move_y = 0
+            current_dir = Direction.up
+        elif current_dir == Direction.up and (
+                    current_x + move_x < 0 or spiral[current_x + move_x][current_y + move_y] != 0):
+            move_x = 0
+            move_y = -1
+            current_dir = Direction.left
+        current_x += move_x
+        current_y += move_y
+        pprint.pprint(spiral)
+        print(current_x, current_y, move_x, move_y, spiral[current_x][current_y], current_dir)
+
+
+create_spiral()
 
 
 
