2b90b26450f695e8b3705f5948e741c0265a3460
Konstantinos Vasileiou <bindigit1@gmail.com>
2015-11-02 16:14:50 +0200

    commiting changes

--- Problem 145/reversible_below_one_billion.py
+++ Problem 145/reversible_below_one_billion.py
@@ -1,9 +1,14 @@
 
 def is_reversible(number):
     string_repr = str(number)
-    if (string_repr[-1] =='0') or (int(string_repr[-1]) % 2 == 1 and int(string_repr[0]) % 2 == 1) or ((int(string_repr[-1]) % 2 == 0 and int(string_repr[0]) % 2 == 1)):
+    if (string_repr[-1] =='0') or (int(string_repr[-1]) % 2 == 1 and int(string_repr[0]) % 2 == 1) or ((int(string_repr[-1]) % 2 == 0 and int(string_repr[0]) % 2 == 0)):
         return False
+    sum = number + int(string_repr[:-1])
+    print(sum)
+    for i in str(sum):
+        print()
+    return True
 
 
 
-print(is_reversible(123450))
+print(is_reversible(36))
