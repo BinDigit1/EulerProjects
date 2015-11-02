49be8740c28c70cd363b8a5482e25cfbe1268021
Konstantinos Vasileiou <bindigit1@gmail.com>
2015-11-02 13:36:22 +0200

    commiting changes

--- Problem 27/quadratic_primes.py
+++ Problem 27/quadratic_primes.py
@@ -1,8 +1,22 @@
+import MathFunctions
+
 def function_generator(a, b):
     def f(h):
         return h*h + a*h + b
     return f
 
+def function_tester(f):
+    count = 0
+    for n in range(0,100):
+        if not MathFunctions.miller_is_prime(f(n)):
+            break
+        count+=1
+    return count
+
+
+for a in range(-1000,1000):
+    for b in range(-1000, 1000):
+        current_function = function_generator(a,b)
 
 
 
