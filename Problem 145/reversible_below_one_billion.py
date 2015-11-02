af7cb22050e1693a54adc2d615e74cc9ab96e4b9
Konstantinos Vasileiou <bindigit1@gmail.com>
2015-11-02 17:35:24 +0200

    commiting changes

--- Problem 145/reversible_below_one_billion.py
+++ Problem 145/reversible_below_one_billion.py
@@ -3,25 +3,29 @@ import time
 def is_reversible(number):
     string_repr = str(number)
 
+
     if (string_repr[-1] =='0'):
         return False
 
     sum_of_digits = 0
     for i in string_repr:
         sum_of_digits += (ord(i) - ord('0'))
-
-    if (sum_of_digits%2) == 0:
-        return False
+    print(sum_of_digits)
+    #
+    # if (sum_of_digits%2) == 0:
+    #     return False
 
 
     sum_of_both = (number+int(string_repr[::-1]))
     for i in str(sum_of_both):
-        if int(i)%2 ==1 :
+        # print(i)
+        if int(i)%2 ==0 :
             return False
-    print(number, int(string_repr[::-1]), sum_of_both)
+    # print(number, int(string_repr[::-1]), sum_of_both)
     return True
 
 start_time = time.time()
 
 print(sum(1 for i in range(1,1000) if is_reversible(i)))
+# print(is_reversible(619))
 print("--- %s seconds ---" % (time.time() - start_time))
