f4349cb527120e740a585aaf34584d045d129b8a
Konstantinos Vasileiou <bindigit1@gmail.com>
2015-11-02 13:54:12 +0200

    commiting changes

--- Problem 27/quadratic_primes.py
+++ Problem 27/quadratic_primes.py
@@ -8,17 +8,24 @@ def function_generator(a, b):
 def function_tester(f):
     count = 0
     for n in range(0,100):
-        if not MathFunctions.miller_is_prime(f(n)):
+        if not MathFunctions.miller_is_prime(abs(f(n))):
             break
         count+=1
     return count
 
-
-for a in range(0,1000):
-    for b in range(0, 1000):
+current_max, product = 0 , 0
+for a in range(-1000,1000):
+    for b in range(-1000, 1000):
         current_function = function_generator(a,b)
-        if function_tester(current_function)>1:
-            print(a, b, function_tester(current_function))
+        number_of_primes_now = function_tester(current_function)
+        if function_tester(current_function)>current_max:
+            current_max = number_of_primes_now
+            print("current max combo: ", a, b, current_max)
+            product = a*b
+print(product)
+
+
+
 
 
 
