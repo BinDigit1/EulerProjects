525f46195b376dc7867c112ebc45991dbc74f942
Konstantinos Vasileiou <bindigit1@gmail.com>
2015-11-03 17:35:53 +0200

    commiting changes

--- Problem 145/reversible_below_one_billion.py
+++ Problem 145/reversible_below_one_billion.py
@@ -1,27 +1,25 @@
-
 import time, math
+
+
 def count_reversibles(limit):
     counter = 0
-    even_set = set(['0','2','4','6','8'])
-    odd_set = set(['1','3','5','7','9'])
-
+    even_set = set(['0', '2', '4', '6', '8'])
+    odd_set = set(['1', '3', '5', '7', '9'])
 
     for number in range(9, limit, 2):
 
-        last_digit = number%10
+        last_digit = number % 10
         if (last_digit == 0):
             continue
         rev_num = int(str(number)[::-1])
-        sum_of_both = (number+rev_num)
+        sum_of_both = (number + rev_num)
         if set(str(sum_of_both)) & even_set != set():
             # print(string_repr, rev_num, sum_of_both)
             continue
-        counter+=2
-
-
-
+        counter += 2
 
     return counter
+
 
 start_time = time.time()
 
