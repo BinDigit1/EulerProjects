889e8a001696a87beb3effed3610a5955ac9ce02
Konstantinos Vasileiou <bindigit1@gmail.com>
2015-11-09 18:18:46 +0200

    Merge branch 'master' of https://github.com/BinDigit1/EulerProjects

--- Problem 18/maximum_path_sum_I.py
+++ Problem 18/maximum_path_sum_I.py
@@ -1,6 +1,18 @@
 
-
-with open("triangle.txt") as f:
+with open("small_triangle.txt") as f:
     content = f.readlines()
+    list_normal, reversed_list = list(), list()
     for x in content:
-        print(x.strip("\n").split(" "))
+        list_normal.append((x.strip("\n").split(" ")))
+    for x in reversed(content):
+        reversed_list.append((x.strip("\n").split(" ")))
+
+def take_first_column(list_a, after_row=0):
+    temp = [row[0] for row in list_a]
+    return temp[after_row:]
+def sum_of_items(list_a):
+    return sum(int(i) for i in list_a)
+
+print(list_normal)
+print(take_first_column(list_normal,1))
+print(sum_of_items(take_first_column(list_normal)))
