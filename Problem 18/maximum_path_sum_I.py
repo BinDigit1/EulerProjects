5aa46a33113063458f5c7ac654a3a3cc535f6cd0
Konstantinos Vasileiou <bindigit1@gmail.com>
2015-11-10 18:23:12 +0200

    Merge branch 'master' of https://github.com/BinDigit1/EulerProjects

--- Problem 18/maximum_path_sum_I.py
+++ Problem 18/maximum_path_sum_I.py
@@ -8,7 +8,9 @@ with file_stream as f:
         list_normal.append((x.strip("\n").split(" ")))
         num_lines += 1
     for x in content:
-        list_normal.append((x.strip("\n").split(" ")))
+        inv = (x.strip("\n").split(" "))
+        inv.reverse()
+        reversed_list.append(inv)
 
 def take_nth_column(list_a, n=0, after_row=0):
     temp = [row[n] for row in list_a][after_row:]
@@ -18,13 +20,14 @@ def sum_of_items(list_a):
 
 level = 1
 left_index, right_index = 0, 0
-print( reversed_list)
-# for i in range(1, num_lines):
-#     level = i
-#     if sum_of_items(take_nth_column(list_normal,left_index, level)) >  sum_of_items(take_nth_column(reversed_list,right_index, level)):
-#         print(take_nth_column(list_normal,left_index, level)[0])
-#         right_index += 1
-#     else:
-#         print(take_nth_column(reversed_list,right_index, level)[0])
-#         left_index += 1
+for i in range(1, num_lines):
+    level = i
+    if sum_of_items(take_nth_column(list_normal,left_index, level)) >  sum_of_items(take_nth_column(reversed_list,right_index, level)):
+        print(take_nth_column(list_normal,left_index, level)[0])
+        list_normal.pop(level)
+        right_index += 1
+    else:
+        print(take_nth_column(reversed_list,right_index, level)[0])
+        reversed_list.pop(level)
+        left_index += 1
 
