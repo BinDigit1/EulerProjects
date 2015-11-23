ef7047bbede4e917937234f3bf15060581c4b4fa
Konstantinos Vasileiou <bindigit1@gmail.com>
2015-11-23 16:37:41 +0200

    more solutions

--- Problem 39/integer_right_triangles.py
+++ Problem 39/integer_right_triangles.py
@@ -1,12 +1,26 @@
-def return_triads(perimeter):
-    triads =set()
-    for x in range(3,perimeter):
-        for y in range(2,perimeter):
-            for z in range(5,perimeter):
-                if x+y+z==perimeter and pow(x,2) + pow(y,2) == pow(z,2):
+import time
 
-                    print(x, y, z)
-                    triads.add({x,y,z})
+def return_triads(perimeter):
+    triads =0
+    for x in range(2,perimeter):
+        for y in range(x+1,perimeter-x):
+            z = perimeter - x -y
+            if (x*y*z)%60 ==0 and (pow(x,2) +pow(y,2) == pow(z,2)):
+                triads +=1
+
     return triads
 
-print(return_triads(120))
+start_time = time.time()
+# print(return_triads(120))
+max_triads, max_triads_index = 0,0
+for i in range(1,1001):
+
+    current_count = return_triads(i)
+    if current_count > max_triads:
+
+        max_triads = current_count
+        max_triads_index = i
+        print(i, max_triads)
+print("max: ", max_triads_index)
+
+print("--- %s seconds ---" % (time.time() - start_time))
